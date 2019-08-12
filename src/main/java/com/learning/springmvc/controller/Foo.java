c4a29540b56d6a09293d9746208df4aed0b90c3d
Pavan <pavan.narasimha@dish.com>
2019-08-12 09:30:39 +0530

    Added model

---
+++ src/main/java/com/learning/springmvc/controller/Foo.java
@@ -0,0 +1,6 @@
+package com.learning.springmvc.controller;
+
+public class Foo {
+    private long id;
+    public String name;
+}
--- src/main/java/com/learning/springmvc/controller/HelloController.java
+++ src/main/java/com/learning/springmvc/controller/HelloController.java
@@ -1,16 +1,19 @@
 package com.learning.springmvc.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class HelloController {
 
-	@GetMapping("/hello")
-	public String hello(Model model) {
+	@PostMapping(value = "/hello", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String hello(@RequestBody Foo foo, Model model) {
 
-		model.addAttribute("name", "John Doe");
+		model.addAttribute("name", foo.name);
 
 		return "welcome";
 	}
